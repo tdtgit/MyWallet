49df96e4ed3bdb28363655fd7001dd95a4454624
Anh Tuấn <mrsugarvn@gmail.com>
2018-05-07 01:35:53 +0700

    Change UIViewController to TableViewController

--- MyWallet/Controllers/Category/CategoryAddViewController.swift
+++ MyWallet/Controllers/Category/CategoryAddViewController.swift
@@ -1,72 +1,138 @@
 //
-//  CategoryAddViewController.swift
+//  CategoryAddTest.swift
 //  MyWallet
 //
-//  Created by Anh Tuan on 4/30/18.
+//  Created by Anh Tuan on 5/7/18.
 //  Copyright © 2018 Anh Tuan. All rights reserved.
 //
 
 import UIKit
-import FirebaseFirestore
 
-class CategoryAddViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    var db = Firestore.firestore()
+class CategoryAddViewController: UITableViewController {
+    
+    @IBOutlet var typeTable: UITableView!
     
     @IBOutlet weak var typeName: UITextField!
     @IBOutlet weak var typeDetail: UITextField!
     
-    @IBOutlet weak var typeTableView: UITableView!
-    
-    
-    @IBAction func add(_ sender: Any) {
-        let newType = WalletType(ID: "", Name: typeName.text!, Detail: typeDetail.text, Section: (typeTableView.indexPathForSelectedRow?.row)!)
-        newType.save()
-        self.navigationController?.popViewController(animated: true)
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1 {
+            return WalletTypeSection.count
+        } else {
+            return super.tableView(tableView, numberOfRowsInSection: section)
+        }
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 1 {
+            let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+            cell.textLabel?.text = WalletTypeSection[indexPath.row]
+            return cell
+        } else {
+            return super.tableView(tableView, cellForRowAt: indexPath)
+        }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return WalletTypeSection.count
-    }
-    
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
             cell.accessoryType = .none
         }
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
             cell.accessoryType = .checkmark
         }
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = WalletTypeSection[indexPath.row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        if indexPath.row == 0 {
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.section == 1 && indexPath.row == 0 {
             cell.accessoryType = .checkmark
             tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
         }
     }
     
+    @IBAction func add(_ sender: Any) {
+        addNewWalletType()
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func addNewWalletType() {
+        let newType = WalletType(ID: "", Name: typeName.text!, Detail: typeDetail.text, Section: (typeTable.indexPathForSelectedRow?.row)!)
+        newType.save()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 0
+//    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
 
     /*
     // MARK: - Navigation
